b1a3acb7fb157c8fd9d4a5f4a5760b80acdc41dc
Bouftioo <ledez.al@gmail.com>
2019-12-20 15:38:32 +0100

    Début tests unitaires testLigue

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -22,4 +22,30 @@ class testLigue
 		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
 		assertEquals(employe, ligue.getEmployes().first());
 	}
+	
+	@Test
+	void  getAdministrateur()
+	{
+		Ligue ligue = new Ligue("Fléchettes");
+		Employe employe = GestionPersonnel.getGestionPersonnel().getRoot();
+		assertEquals(employe, ligue.getAdministrateur());
+	}
+	@Test
+	void remove()
+	{
+		Ligue ligue = new Ligue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
+		employe.remove();
+		assertFalse(ligue.getEmployes().contains(employe));
+	}
+	
+	@Test
+	void compareTo()
+	{
+		Ligue ligue = new Ligue("Fléchettes");
+		Ligue ligue2 = new Ligue("Fortnite");
+		ligue.getNom().compareTo(ligue2.getNom());
+		assertNotEquals(ligue.getNom(), ligue2.getNom());
+	}
+	
 }
