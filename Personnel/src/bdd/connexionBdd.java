efc266aced1a35d37839c54d7fb2161c0b2d302c
Alexandre Le Dez <ledez.al@gmail.com>
2020-03-19 21:40:57 +0100

    Changement bdd

--- Personnel/src/bdd/connexionBdd.java
+++ Personnel/src/bdd/connexionBdd.java
@@ -1,39 +1,47 @@
 package bdd;
 
 import java.awt.EventQueue;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.util.*;
 
+
+
+import java.sql.*;
+import java.sql.Date;
+import java.io.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import personnel.Employe;
 import personnel.GestionPersonnel;
+import personnel.Ligue;
 import personnel.Passerelle;
 import personnel.SauvegardeImpossible;
 
 public class connexionBdd implements Passerelle{
-    
-    public static void main(String[] args) {
-    	String BDD = "personnel";
-    	String port = "3308";
-        String url = "jdbc:mysql://localhost:"+ port + "/" + BDD + "?serverTimezone=UTC";
-        String user = "root";
-        String password = "";
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-            	Connection conn = null;
-            	
-                try {
-                	conn = DriverManager.getConnection(url, user, password);
-                	System.out.println("T'es co bg");
-                }
-                catch (Exception e)
-                {
-                	e.printStackTrace();
-                	System.out.println("Erreur");
-                	System.exit(0);
-                }
-            }
-        });
+	private static Connection myConn ;
+	
+    public static void main(String[] args) throws Exception{
+    	
+    	
+    	myConn = null;
+    	
+    		
+    	 Properties props = new Properties();
+         props.load(new FileInputStream("demo.properties"));
+         
+         String user = props.getProperty("user");
+         String password = props.getProperty("password");
+         String dburl = props.getProperty("dburl");
+         
+         myConn = DriverManager.getConnection(dburl, user, password);
+         
+         System.out.println("Connecté à la BDD");
+        
+    	
+        
     }
 
+    
+   
 	@Override
 	public GestionPersonnel getGestionPersonnel() {
 		// TODO Auto-generated method stub
